6e9cffad8997b8d842d5b4663303995fe3840827
Niilo Sirola <niilo.sirola@exafore.com>
2018-05-16 09:30:13 +0300

    Add get_msm_gps_prn/code etc. in rtcm3_msm_utils.h

--- c/include/rtcm3_msm_utils.h
+++ c/include/rtcm3_msm_utils.h
@@ -23,4 +23,17 @@ msm_enum to_msm_type(uint16_t msg_num);
 constellation_t to_constellation(uint16_t msg_num);
 uint8_t count_mask_bits(uint16_t mask_size, const bool mask[]);
 
+uint8_t get_msm_gps_prn(uint8_t sat_id);
+code_t get_msm_gps_code(uint8_t signal_id);
+uint8_t get_msm_glo_prn(uint8_t sat_id);
+code_t get_msm_glo_code(uint8_t signal_id);
+uint8_t get_msm_gal_prn(uint8_t sat_id);
+code_t get_msm_gal_code(uint8_t signal_id);
+uint8_t get_msm_qzs_prn(uint8_t sat_id);
+code_t get_msm_qzs_code(uint8_t signal_id);
+uint8_t get_msm_bds2_prn(uint8_t sat_id);
+code_t get_msm_bds2_code(uint8_t signal_id);
+uint8_t get_msm_sbas_prn(uint8_t sat_id);
+code_t get_msm_sbas_code(uint8_t signal_id);
+
 #endif /* SWIFTNAV_RTCM3_MSM_UTILS_H */
