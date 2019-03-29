14b03ba3379b0e78f5e25c0f810f2d7f61f628d4
pgrgich <pgrgich@swift-nav.com>
2019-03-29 12:00:07 -0700

    Add output.

--- c/src/decode.c
+++ c/src/decode.c
@@ -312,7 +312,7 @@ uint8_t construct_L1_code(rtcm_freq_data *l1_freq_data,
   if (pr != (int)PR_L1_INVALID) {
     return 1;
   }
-  fprintf(stderr, "Invalid L1 code\n");
+  fprintf(stdout, "Invalid L1 code\n");
   return 0;
 }
 
@@ -324,7 +324,7 @@ uint8_t construct_L1_phase(rtcm_freq_data *l1_freq_data,
   if (phr_pr_diff != (int)CP_INVALID) {
     return 1;
   }
-  fprintf(stderr, "Invalid L1 phase\n");
+  fprintf(stdout, "Invalid L1 phase\n");
   return 0;
 }
 
@@ -335,7 +335,7 @@ uint8_t construct_L2_code(rtcm_freq_data *l2_freq_data,
   if (pr != (int)PR_L2_INVALID) {
     return 1;
   }
-  fprintf(stderr, "Invalid L2 code\n");
+  fprintf(stdout, "Invalid L2 code\n");
   return 0;
 }
 
@@ -348,7 +348,7 @@ uint8_t construct_L2_phase(rtcm_freq_data *l2_freq_data,
   if (phr_pr_diff != (int)CP_INVALID) {
     return 1;
   }
-  fprintf(stderr, "Invalid L2 phase\n");
+  fprintf(stdout, "Invalid L2 phase\n");
   return 0;
 }
 
@@ -525,7 +525,7 @@ rtcm3_rc rtcm3_decode_1004(const uint8_t buff[], rtcm_obs_message *msg_1004) {
     return RC_INVALID_MESSAGE;
   }
 
-  fprintf(stderr, "Decoded %u GPS sats\n", msg_1004->header.n_sat);
+  fprintf(stdout, "Decoded %u GPS sats\n", msg_1004->header.n_sat);
   for (uint8_t i = 0; i < msg_1004->header.n_sat; i++) {
     init_sat_data(&msg_1004->sats[i]);
 
@@ -1181,6 +1181,7 @@ static rtcm3_rc rtcm3_decode_msm_internal(const uint8_t buff[],
 
   uint16_t bit = 0;
   bit += rtcm3_read_msm_header(buff, cons, &msg->header);
+    fprintf(stdout, "Epoch: %u\n", msg->header.tow_ms);
 
   if (RTCM_CONSTELLATION_GLO != cons) {
     if (msg->header.tow_ms > RTCM_MAX_TOW_MS) {
